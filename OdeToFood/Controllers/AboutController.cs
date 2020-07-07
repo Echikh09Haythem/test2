969add1e8fb7a33aa0443d157a1e8144b690c730
Echikh09Haythem <echikh.haythem@esidoctor.com>
2020-07-07 13:09:40 +0200

    first commit

---
+++ OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    [Route("company/[controller]/[action]")]
+    public class AboutController
+    {
+        public string Phone()
+        {
+            return "1+55+22222+5555";
+        }
+        public string Adress()
+        {
+            return "USA";
+        }
+    }
+}
